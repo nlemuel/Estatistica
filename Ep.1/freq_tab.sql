51904a828d473e56ee8bfec8e7d09bd7e7c4afb0
nlemuel <lemuelayres@gmail.com>
2024-09-30 20:00:27 -0300

    feat: add freq table sql

---
+++ Ep.1/freq_tab.sql
@@ -0,0 +1,13 @@
+WITH tb_freq AS (
+
+    SELECT DISTINCT seller_state,
+        COUNT(*) AS freqAbsoluta
+    FROM tb_sellers
+    GROUP BY seller_state
+
+)
+
+SELECT *,
+       CAST(freqAbsoluta AS FLOAT) / (SELECT SUM(CAST(freqAbsoluta AS FLOAT)) FROM tb_freq) AS freqRelativa,
+       SUM(CAST(freqAbsoluta AS FLOAT) / (SELECT SUM(CAST(freqAbsoluta AS FLOAT)) FROM tb_freq)) OVER (ORDER BY freqAbsoluta DESC) AS freqRelativaAcum
+FROM tb_freq
